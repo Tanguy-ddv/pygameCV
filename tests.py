5a8f6e0353accfc222b1de7ebc50870760d5362a
Tanguy-ddv <tanguy.dugas01@gmail.com>
2025-01-25 17:36:06 +0100

    new: add code to take alpha into account

--- tests.py
+++ tests.py
@@ -1,16 +1,35 @@
 
 from pygamecv.drawing import circle, ellipse, arc, pie
-from pygame import image
+from pygame import image, display
 import os
-img = image.load("Lenna.png")
-dir = "test_results"
-os.makedirs(dir, exist_ok=True)
-img = circle(img, (100, 200), 100, (255, 255, 0, 255), 50, True)
-img = circle(img, (200, 100), 100, (255, 0, 0, 255), 0, False)
-img = circle(img, (300, 300), 70, (0, 255, 0, 255), 50, False)
-img = circle(img, (400, 150), 100, (255, 0, 255, 255), 0, True)
-#img = circle(img, (0, 0), 100, (0, 255, 255, 255), 50, True)
-img = circle(img, (500, 500), 100, (0, 255, 255, 255), 50, True)
-img = ellipse(img, (0, 0), 50, 100, (0, 255, 255, 255), 50, True)
 
-image.save(img, os.path.join(dir, "circle.png"))
+def init(save_dir):
+    display.init()
+    display.set_mode((100, 100))
+    img = image.load("Lenna.png")
+    img_alpha = image.load("Lenna_alpha.png")
+    os.makedirs(save_dir, exist_ok=True)
+    return img, img_alpha
+
+def test_circle(save_dir):
+    img, img_alpha = init(save_dir)
+    img = circle(img, (100, 200), 100, (255, 255, 0, 255), 50, True)
+    img = circle(img, (200, 100), 100, (255, 0, 0, 255), 0, False)
+    img = circle(img, (300, 300), 70, (0, 255, 0, 255), 50, False)
+    img = circle(img, (400, 150), 100, (255, 0, 255, 255), 0, True)
+    img = circle(img, (0, 0), 100, (0, 255, 255, 255), 50, True)
+    img = circle(img, (500, 500), 100, (0, 255, 255, 255), 50, True)
+
+    img_alpha = circle(img_alpha, (100, 200), 100, (255, 255, 0, 255), 50, True)
+    img_alpha = circle(img_alpha, (200, 100), 100, (255, 0, 0, 255), 0, False)
+    img_alpha = circle(img_alpha, (300, 300), 70, (0, 255, 0, 100), 50, False)
+    img_alpha = circle(img_alpha, (400, 150), 100, (255, 0, 255, 255), 0, True)
+    img_alpha = circle(img_alpha, (0, 0), 100, (0, 255, 255, 255), 50, True)
+    img_alpha = circle(img_alpha, (500, 500), 100, (0, 255, 255, 255), 50, True)
+    img_alpha = circle(img_alpha, (450, 400), 50, (0, 0, 255, 125), 0, True)
+
+    image.save(img, os.path.join(save_dir, "circles.png"))
+    image.save(img_alpha, os.path.join(save_dir, "circles_alpha.png"))
+
+save_dir = "test_results"
+test_circle(save_dir)
