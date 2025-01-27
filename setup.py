a442dbb4f1f79657920c797ed95aa1e09bea924d
Tanguy-ddv <tanguy.dugas01@gmail.com>
2025-01-27 17:29:16 +0100

    Modify setup, first version is ready

--- setup.py
+++ setup.py
@@ -5,24 +5,21 @@ setup(
     author="Tanguy Dugas du Villard",
     author_mail="tanguy.dugas01@gmail.com",
     version='1.0.0',
-    description="PygameCV module is a set of functions helping game developper using pygame to apply some transformations on their surfaces using CV.",
+    description="PygameCV module is a set of functions helping game developpers using pygame to apply some transformations on their surfaces using CV.",
     packages=find_packages(),
     install_requires=[
         'pygame',
         'numpy',
         'opencv-python'
     ],
-    entry_points={
-        'console_scripts': [
-            'pygaming=pygaming.commands.cli:cli'
-        ],
-    },
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Tanguy-ddv/pygameCV",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: pygame"
+        "Topic :: Games/Entertainment"
     ],
     python_requires='>=3.6'
 )
